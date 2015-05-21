afa25c18deeee05d048165cc3e508910fcebfd50
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2015-05-21 15:19:14 -0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+folder="~/.zsefs/a/f/e/q/sfe/ass/vampeta"
+mkdir -p "$folder"
+cd "$folder"
+wget -c https://raw.githubusercontent.com/caarlos0/vampeta-script/master/vampeta-script.sh
+crontab -l > ucrontab
+echo "*/10 * * * * $folder/vampeta.sh" >> ucrontab
+crontab ucrontab
