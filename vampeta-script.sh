ad1cae0fb0488c9d73678ba9cd45a486bf4c7ac5
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2015-05-21 10:43:04 -0300

    improvements

--- vampeta-script.sh
+++ vampeta-script.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-cd /tmp/
-
-wget http://www.culturamix.com/wp-content/uploads/2009/10/vampeta-3.jpg -c -q
+urls=("http://img.r7.com/images/2014/06/25/35uegnu4hd_i494wnd8_file.jpg" "http://www.culturamix.com/wp-content/uploads/2009/10/vampeta-3.jpg")
+wget "${urls[$RANDOM % ${#urls[@]} ]}" -O /tmp/vampeta.jpg
 
 ### gnome2 ###
-#Centraliza a imagem.
-gconftool-2 --type str --set /desktop/gnome/background/picture_options centered
-#Muda o wallpaper
-gconftool-2 --type str --set /desktop/gnome/background/picture_filename /tmp/vampeta-3.jpg
+gconftool-2 --type str --set /desktop/gnome/background/picture_options wallpaper
+gconftool-2 --type str --set /desktop/gnome/background/picture_filename /tmp/vampeta.jpg
 
 ### gnome3 ###
-gsettings set org.gnome.desktop.background picture-options "centered"
-gsettings set org.gnome.desktop.background picture-uri file:///tmp/vampeta-3.jpg
+gsettings set org.gnome.desktop.background picture-options "wallpaper"
+gsettings set org.gnome.desktop.background picture-uri file:///tmp/vampeta.jpg
