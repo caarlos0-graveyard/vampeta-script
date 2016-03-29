b63044ff2e4138ca3b182e5d57a4d1554c78fc33
Felipe Sabino <fsabino@users.noreply.github.com>
2016-03-28 21:49:55 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 folder="$HOME/.zsefs/a/f/e/q/sfe/ass/vampeta"
 mkdir -p "$folder"
 cd "$folder"
-curl -s https://raw.githubusercontent.com/caarlos0/vampeta-script/master/vampeta-script.sh
+curl -s https://raw.githubusercontent.com/caarlos0/vampeta-script/master/vampeta-script.sh > vampeta-script.sh
 chmod +x vampeta-script.sh
 crontab -l > ucrontab
 echo "*/10 * * * * bash $folder/vampeta-script.sh >> /tmp/v.log 2>&1" >> ucrontab
